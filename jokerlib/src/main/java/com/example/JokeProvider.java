82368d4ecf21ce7985163c30e490eb10b4c0d7e6
Guillermo Gutierrez <guillermo20@gmail.com>
2017-01-24 21:56:50 -0500

    added a Java Library that provides the jokes.

--- jokerlib/src/main/java/com/example/JokeProvider.java
+++ jokerlib/src/main/java/com/example/JokeProvider.java
@@ -1,4 +1,16 @@
 package com.example;
 
+import java.util.Random;
+
 public class JokeProvider {
+
+    Random rd = new Random();
+
+    private String[] jokeV = {"What did the ocean say to the beach? Nothing, it just waved."
+            , "A neutron walks into a bar and asks, how much for a beer? The bartender says, for you? no charge."
+            , "Why did the chicken cross the mobius strip? To get to the same side."};
+
+    public String getRandomJoke(){
+        return jokeV[rd.nextInt(jokeV.length)];
+    }
 }

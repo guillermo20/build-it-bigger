f4f5f3d6fa8a76070b102fdf23bbde52a25ebd1b
Guillermo Gutierrez <guillermo20@gmail.com>
2017-01-25 22:45:25 -0500

    finished the activity where the jokes are displayed (android library)

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.JokeProvider;
+import com.example.jokerandroidlib.JokeDisplayActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -47,7 +48,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, jokeProvider.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeDisplayActivity.class);
+        intent.putExtra("joke",jokeProvider.getRandomJoke());
+        startActivity(intent);
     }
 
 
--- jokerandroidlib/src/main/java/com/example/jokerandroidlib/JokeDisplayActivity.java
+++ jokerandroidlib/src/main/java/com/example/jokerandroidlib/JokeDisplayActivity.java
@@ -1,13 +1,24 @@
 package com.example.jokerandroidlib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class JokeDisplayActivity extends AppCompatActivity {
+
+    private final String TAG = JokeDisplayActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_display);
+        String joke = getIntent().getStringExtra("joke");
+        Log.i(TAG,joke);
+        TextView  textView = (TextView) findViewById(R.id.jokedisplay_textview);
+        textView.setText(joke);
     }
+
+
+
 }
